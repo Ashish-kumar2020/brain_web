5290a73ebf8b64966fd56a416a75ed8713b13a30
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-12-21 21:21:52 +0530

    implemented signup and signin api for admin and user

--- Route/adminRoute.js
+++ Route/adminRoute.js
@@ -1,17 +1,38 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../DbSchema");
+const { Types } = require("mongoose");
 
-adminRouter.post("/signup", (req, res) => {
-  res.status(201).json({
-    message: "Admin Added Successfully",
+adminRouter.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  const user = await adminModel.create({
+    email,
+    password,
+    firstName,
+    lastName,
   });
+  if (user) {
+    res.status(201).json({
+      message: "Admin Added Successfully",
+      user: user,
+    });
+  }
 });
 
-adminRouter.post("/signin", (req, res) => {
-  res.status(201).json({
-    message: "Admin Logged in Successfully",
-  });
+adminRouter.post("/signin", async (req, res) => {
+  const { email, password, userId } = req.body;
+  const objectID = new Types.ObjectId(userId);
+  const userFound = await adminModel.findOne({ _id: objectID });
+  if (userFound) {
+    res.status(201).json({
+      message: "Admin Logged in Successfully",
+      userFound: userFound,
+    });
+  } else {
+    res.status(201).json({
+      message: "No User Found, Please signup",
+    });
+  }
 });
 
 adminRouter.post("/course", (req, res) => {
--- Route/userRouter.js
+++ Route/userRouter.js
@@ -1,17 +1,38 @@
 const { Router } = require("express");
+const { userModel } = require("../DbSchema");
+const { Types } = require("mongoose");
 const userRouter = Router();
 
-userRouter.post("/signup", (req, res) => {
+userRouter.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  const user = await userModel.create({
+    email,
+    password,
+    firstName,
+    lastName,
+  });
   res.status(201).json({
     message: "User Added Successfully",
+    user: user,
   });
 });
 
-// User Signup ROuter
-userRouter.post("/signin", (req, res) => {
-  res.status(201).json({
-    message: "User Signed in Successfully",
-  });
+// User Signin Router
+userRouter.post("/signin", async (req, res) => {
+  const { email, password, userId } = req.body;
+  const objectID = new Types.ObjectId(userId);
+  const data = await userModel.findOne({ _id: objectID });
+  console.log(data.email);
+  if (!data) {
+    res.status(201).json({
+      message: "User not found please signup",
+    });
+  } else {
+    res.status(201).json({
+      message: "User Signed in Successfully",
+      data: data,
+    });
+  }
 });
 
 // See all the courses
